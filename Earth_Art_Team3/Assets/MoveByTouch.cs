0a98ea693d3370ecca67d8b844a21ef31bd980fd
Pix <w853245475@gmail.com>
2020-10-02 10:31:29 -0600

    cellphone input test

---
+++ Earth_Art_Team3/Assets/MoveByTouch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            for (int i = 0;  i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(0);
+                Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+                Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+            }
+
+
+        }
+    }
+}
