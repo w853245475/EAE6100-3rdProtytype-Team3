268dcfedb3b9da84c26d10444a50bd82097d7790
Pix <w853245475@gmail.com>
2020-10-02 22:20:59 -0600

    Basic Camera Movement

---
+++ Earth_Art_Team3/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    Vector3 touchStart;
+    public float minZoom = 1;
+    public float maxZoom = 15;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(touchStart);
+        }
+        if(Input.touchCount >= 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position = touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+            zoom(-difference * 0.01f);
+        }
+
+        else if(Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Camera.main.transform.position += direction;
+        }
+
+
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, minZoom, maxZoom);
+    }
+
+}
