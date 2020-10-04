f7e4c4330ff7786f7549f31d9b7268707a8d4762
Pix <w853245475@gmail.com>
2020-10-03 19:58:24 -0600

    Place Plant Done, Next Thing To make Plant Grow

--- Earth_Art_Team3/Assets/Scripts/MoveCamera.cs
+++ Earth_Art_Team3/Assets/Scripts/MoveCamera.cs
@@ -8,37 +8,45 @@ public class MoveCamera : MonoBehaviour
     public float minZoom = 1;
     public float maxZoom = 15;
 
+    public float minX, minY, maxX, maxY;
+    public bool CanMoveCamera = true;
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(CanMoveCamera)
         {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(touchStart);
+            if(Input.GetMouseButtonDown(0))
+            {
+                touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //Debug.Log(touchStart);
+            }
+            if(Input.touchCount >= 2)
+            {
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position = touchOne.deltaPosition;
+
+                float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+                float difference = currentMagnitude - prevMagnitude;
+                zoom(difference * 0.01f);
+            }
+
+            else if(Input.GetMouseButton(0))
+            {
+                Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+                
+                Camera.main.transform.position += direction;
+                
+                
+                //Camera.main.transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY), -10);
+            }
+            zoom(Input.GetAxis("Mouse ScrollWheel"));
         }
-        if(Input.touchCount >= 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
-
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position = touchOne.deltaPosition;
-
-            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
-
-            float difference = currentMagnitude - prevMagnitude;
-            zoom(-difference * 0.01f);
-        }
-
-        else if(Input.GetMouseButton(0))
-        {
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            Camera.main.transform.position += direction;
-        }
-
-
-        zoom(Input.GetAxis("Mouse ScrollWheel"));
     }
 
     void zoom(float increment)
---
+++ Earth_Art_Team3/Assets/Scripts/ObjectCaller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ObjectCaller : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler
+{
+    public GameObject objectDrag;
+    public GameObject objectGame;
+    public Canvas canvas;
+    private GameObject objectDragInstance;
+    public void OnDrag(PointerEventData eventData)
+    {
+        objectDragInstance.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        objectDragInstance.transform.position = new Vector3(objectDragInstance.transform.position.x,
+                                                            objectDragInstance.transform.position.y,
+                                                            10f);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        MoveCamera cam = Camera.main.GetComponent<MoveCamera>();
+        cam.CanMoveCamera = false;
+
+        objectDragInstance = Instantiate(objectDrag);
+        objectDragInstance.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        MoveCamera cam = Camera.main.GetComponent<MoveCamera>();
+        cam.CanMoveCamera = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        MoveCamera cam = Camera.main.GetComponent<MoveCamera>();
+        cam.CanMoveCamera = true;
+        Debug.Log(cam.CanMoveCamera);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
